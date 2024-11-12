0d068ecbb70087901121fe0d5ead42844475afc5
Jakdeveloper1 <88589230+Jakdeveloper1@users.noreply.github.com>
2024-11-12 10:40:17 +0500

    CreateBrowserRouter function added

--- React App/src/index.js
+++ React App/src/index.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App  from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from './Route/error';
+import Contact from './Route/contact';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// using React Routing  V6.4 or greater
+const Route = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+          path:"/home",
+          element:<Contact/>
+      },
+
+    ]
+  },
+  {
+    path:"/contacts/:1",
+    element:<Contact/>
+  }
+])
+
+//React Routing
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Route}></RouterProvider>
   </React.StrictMode>
 );
+
+
+
+
+// Without React Routing 
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App/>
+//   </React.StrictMode>
+// );
+
+
+
+
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

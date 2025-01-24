3d1681d552643ddc42e1ccf9868a15e1c909a74d
Jakdeveloper1 <88589230+Jakdeveloper1@users.noreply.github.com>
2025-01-24 09:26:18 +0500

    Update Main.js

--- React App/src/Zustand/Main.js
+++ React App/src/Zustand/Main.js
@@ -8,7 +8,6 @@ export const Main = ()=>{
     
     return(
         <>
-        </Second>
             <p>Counter: {count}</p>
             <button onClick={incremenetFunction}>Increment</button>
             
